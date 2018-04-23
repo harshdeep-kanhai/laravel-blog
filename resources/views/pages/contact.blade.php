923c42d6863e1daa7619799a0a94dc85501f8bf5
Harshdeep Kanhai <harshdeepkanhai@gmail.com>
2018-04-23 09:24:45 +0000

    add navbar and make Post model

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -1,5 +1,28 @@
 @extends('main')
 
+@section('title', '| Contact')
+
 @section('content')
-    content
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Contact Me</h1>
+            <hr>
+            <form action="">
+                <div class="form-group">
+                    <label for="email">Email:</label>
+                    <input id="email "type="text" name="email" class="form-control"/>
+                </div>
+                <div class="form-group">
+                    <label for="subject">Subject:</label>
+                    <input id="subject" name="subject" class="form-control"/>
+                </div>
+                <div class="form-group">
+                    <label for="message">Message:</label>
+                    <textarea id="message" name="message" class="form-control"/>Type your message here ...
+                    </textarea>
+                </div>
+                <input type="submit" value="Send Message" class="btn btn-success"/>
+            </form>
+        </div>
+    </div>
 @endsection
